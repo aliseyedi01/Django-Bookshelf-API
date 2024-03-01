d04cfa039bddd76a67089cab9c61cd2c651d00f1
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-01 19:11:58 +0330

    🐞 fix(swagger): solve not show swagger in render

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 # drf
 from rest_framework import permissions
 # Swagger
@@ -29,5 +30,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 	path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
 
 
