2775d4ee992717dd0af80faf3668c66826684d9d
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-02 13:47:38 +0330

    ✨ feat(auth): add serialize for user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -4,10 +4,15 @@ from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 # apps
 from .models import User
+import re
 
+
+MAX_NAME_LENGTH = 30
 
 class UserSerializer(serializers.ModelSerializer):
 
+    first_name = serializers.CharField(max_length=MAX_NAME_LENGTH, required=True)
+    last_name = serializers.CharField(max_length=MAX_NAME_LENGTH, required=True)
     password = serializers.CharField(write_only=True, required=True)
     email = serializers.EmailField(required=True)
     username = serializers.CharField(required=True)
@@ -16,6 +21,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ('uuid', 'first_name', 'last_name', 'username', 'email', 'password', 'created_at')
         read_only_fields = ('uuid', 'created_at')
+        extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         validated_data['password'] = make_password(validated_data['password'])
@@ -29,4 +35,33 @@ class UserSerializer(serializers.ModelSerializer):
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
             raise serializers.ValidationError("A user with this username already exists.")
+        return value
+
+    def validate_password(self, value):
+        MIN_LENGTH = 6
+        password_regex = r"(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)"  # Regex for password complexity
+
+        if len(value) < MIN_LENGTH:
+            raise serializers.ValidationError(f"Password must be at least {MIN_LENGTH} characters long")
+
+        # Optional check for common password patterns (consider using a third-party library for a more comprehensive list)
+        common_passwords = ["password", "123456", "qwerty"]
+        if value.lower() in common_passwords:
+            raise serializers.ValidationError("Please choose a stronger password. This password is commonly used and vulnerable to attacks.")
+
+        if not re.search(password_regex, value):
+            raise serializers.ValidationError(
+                "Password must include at least one uppercase letter, lowercase letter, number, and symbol"
+            )
+
+        return value
+
+    def validate_first_name(self, value):
+        if len(value) > MAX_NAME_LENGTH:
+            raise serializers.ValidationError(f"First name cannot exceed {MAX_NAME_LENGTH} characters.")
+        return value
+
+    def validate_last_name(self, value):
+        if len(value) > MAX_NAME_LENGTH:
+            raise serializers.ValidationError(f"Last name cannot exceed {MAX_NAME_LENGTH} characters.")
         return value
--- authentication/views.py
+++ authentication/views.py
@@ -9,17 +9,25 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 # apps
 from .serializers import UserSerializer
-
+# swagger
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 class SignUpView(APIView):
     permission_classes = [AllowAny]
-
+    @swagger_auto_schema(
+        request_body=UserSerializer,
+        responses={
+            201:UserSerializer(many=True),
+            400: 'Bad request (e.g., invalid data, missing required fields)',
+        }
+    )
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             # Handle successful signup (e.g., redirect to login page, send confirmation email)
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'message': 'User created successfully.', 'data': serializer.data}, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
