3095af75c774153a067c14dd5f2fb6756d11b127
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-02 19:50:27 +0330

    ✨ feat(auth): verify email with check otp

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-from .views import SignUpView
+from .views import SignUpView , VerifyEmailView
 from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path("verify-email/<slug:username>", VerifyEmailView.as_view() , name="verify-email"),
     path('refresh-token/', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify-token/', TokenVerifyView.as_view(), name='token_verify'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import redirect
 from django.utils import timezone
 from django.core.mail import send_mail
 from django.contrib.messages import success, warning
+from django.shortcuts import get_object_or_404, redirect
+from django.utils.timezone import now
 # drf
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -12,7 +14,7 @@ from rest_framework import status
 from rest_framework.permissions import AllowAny
 # apps
 from .serializers import UserSerializer
-from .models import OtpToken
+from .models import OtpToken,User
 # swagger
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
@@ -38,7 +40,7 @@ class SignUpView(APIView):
             message = f"""
                 Hi {user}, here is your OTP {otp.otp_code}
                 It expires in 5 minutes, use the url below to redirect back to the website
-                http://127.0.0.1:8000/verify-email/{user}
+                http://127.0.0.1:8000/verify-email/{otp.user.username}
             """
             try:
                 send_mail(
@@ -54,3 +56,34 @@ class SignUpView(APIView):
 
             return Response({'Message': 'Account created successfully! An OTP has been sent to your email for verification'} ,status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class VerifyEmailView(APIView):
+
+    def post(self, request, username):
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({'error': f'User with this username : {username} Not Found!'}, status=status.HTTP_404_NOT_FOUND)
+
+        user_otp = OtpToken.objects.filter(user=user).order_by('-created_at').first()
+        if not user_otp:
+            return Response({'error': 'No OTP found for this user'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Validate OTP code
+        if request.data.get('otp_code') != user_otp.otp_code:
+            return Response({'error': 'Invalid OTP code'}, status=status.HTTP_400_BAD_REQUEST)
+
+        # Check for expired OTP
+        if user_otp.expires_at < now():
+            user_otp.delete()
+            return Response({'error': 'OTP has expired. Please request a new one.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user.save()
+        user_otp.delete()
+
+        # Log in the user automatically (optional)
+        # login(request, authenticate(username=username, password=user.password))
+
+        return Response({'Message': 'Your account has been activated successfully!'} ,status=status.HTTP_200_OK)
