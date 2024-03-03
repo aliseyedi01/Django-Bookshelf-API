1e7411a99df7ac69d822ec3d04dedae16bd2d5fd
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-03 23:47:54 +0330

    🐞 fix(cors): add method and header to cors

--- core/settings.py
+++ core/settings.py
@@ -38,12 +38,33 @@ INSTALLED_APPS = [
 ]
 
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:[3000-9000]",
-#     "http://127.0.0.1:[3000-9000]",
-#     "http://localhost:3000",
-# ]
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:[3000-9000]",
+    "http://127.0.0.1:[3000-9000]",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'ACCEPT',
+    'ACCEPT-ENCODING',
+    'AUTHORIZATION',
+    'CONTENT-TYPE',
+    'DNT',
+    'ORIGIN',
+    'USER-AGENT',
+    'X-CSRFTOKEN',
+    # Add other headers as needed
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
