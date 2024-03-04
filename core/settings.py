f42a47fcba503e1ba2fe4e2b1f3dfe90357b3675
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-04 17:12:20 +0330

    ✨ feat(cors): add new origin

--- core/settings.py
+++ core/settings.py
@@ -42,14 +42,17 @@ ALLOWED_HOSTS = [
     "*",
     "127.0.0.1",
     "127.0.0.1:3000",
+    "127.0.0.1:8000",
     "localhost",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://warm-yeot-5f210e.netlify.app",
     "https://library-api-t70g.onrender.com"
 ]
 
 # CORS_ALLOW_ALL_ORIGINS: True
 CORS_ALLOWED_ORIGINS = [
+    "https://warm-yeot-5f210e.netlify.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     "http://localhost:5000",
@@ -57,6 +60,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = [
     "https://127.0.0.1",
+    "https://warm-yeot-5f210e.netlify.app",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
 ]
