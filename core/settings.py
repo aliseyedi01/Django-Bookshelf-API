950d732c408df411908ef3ab8cd00f547fc01583
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-04 13:45:02 +0330

    🐞 fix(cors): set true for credentials

--- core/settings.py
+++ core/settings.py
@@ -38,19 +38,23 @@ INSTALLED_APPS = [
 ]
 
 ALLOWED_HOSTS = ['*']
-CORS_ALLOW_ALL_ORIGINS: True
+# CORS_ALLOW_ALL_ORIGINS: True
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://localhost:[3000-9000]",
-#     "http://127.0.0.1:[3000-9000]",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:[3000-9000]",
+    "http://127.0.0.1:[3000-9000]",
+]
 
-# CSRF_TRUSTED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://localhost:[3000-9000]",
-#     "http://127.0.0.1:[3000-9000]",
-# ]
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:[3000-9000]",
+    "http://127.0.0.1:[3000-9000]",
+]
+
+CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
     'DELETE',
@@ -75,10 +79,10 @@ CORS_ALLOW_HEADERS = [
 
 
 MIDDLEWARE = [
-    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
