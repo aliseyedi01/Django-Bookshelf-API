59e96948c60001269575ab956383f90cc10083c2
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-07 01:04:49 +0330

    ✨ feat(endpoint): change name otp

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,8 +4,8 @@ from .views import  SignUpView , SignInView, SignOutView , VerifyEmailView ,Rese
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path("verify-email/", VerifyEmailView.as_view() , name="verify-email"),
-    path("resend-otp/", ResendOtpView.as_view(), name="resend-otp"),
+    path("otp/check/", VerifyEmailView.as_view() , name="verify-email"),
+    path("otp/resend/", ResendOtpView.as_view(), name="resend-otp"),
     path("signin/", SignInView.as_view(), name="signin"),
     path("signout/", SignOutView.as_view(), name="signout"),
 ]
