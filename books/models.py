f72871a509e1190c6857238af77655fc35f28db6
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-07 02:42:09 +0330

    ✨ feat(model): add category model

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from authentication.models import User
+from categories.models import Category
 
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
@@ -8,6 +9,7 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     image_url = models.URLField(max_length=2000)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='books')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='books',default=1)
 
     is_read = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
