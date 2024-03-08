8aa45cc81c20f11e0e6088fd1b69213216aba931
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-08 18:11:35 +0330

    ✨ feat(swagger): add swagger for this endpoint

--- accounts/views.py
+++ accounts/views.py
@@ -1,32 +1,36 @@
 from django.shortcuts import render
-
-# Create your views here.
-
-
+from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from authentication.models import User
 from .serializers import UserSerializer
-
+from authentication.utils import  SwaggerResponse
 
 class MyProfileView(APIView):
-  permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
+    @extend_schema(
+        description="Retrieve information about the authenticated user's profile",
+        summary="Get user information",
+        responses={
+            200 : UserSerializer,
+            404 : SwaggerResponse.NOT_FOUND,
+        }
+    )
+    def get(self, request):
+        username = request.user.username
 
-  def get(self, request):
-      username = request.user.username
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
 
-      try:
-          user = User.objects.get(username=username)
-      except User.DoesNotExist:
-          return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        serializer = UserSerializer(user)
 
-      serializer = UserSerializer(user)
-
-      return Response({
-          "message": "This is your profile",
-          "data":serializer.data
-          },status=status.HTTP_201_CREATED
-      )
+        return Response({
+            "message": "This is your profile",
+            "data":serializer.data
+            },status=status.HTTP_201_CREATED
+        )
