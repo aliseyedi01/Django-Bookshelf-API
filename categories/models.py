bdd329f47ab5b888ac609bb06eb24f778155715f
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-09 22:28:37 +0330

    ✨ feat(category): remove unique for field name

--- categories/models.py
+++ categories/models.py
@@ -5,7 +5,7 @@ from authentication.models import User
 # Create your models here.
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=50)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='categories')
 
     def __str__(self):
