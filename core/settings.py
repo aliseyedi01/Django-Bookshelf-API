de8ccb336c05aee341c4f2de2a82482d2ec566be
Ali Seyedi <118107025+aliseyedi01@users.noreply.github.com>
2024-03-10 18:46:33 +0330

    ✨ feat(env): add environment variable for email

--- core/settings.py
+++ core/settings.py
@@ -207,11 +207,12 @@ SPECTACULAR_SETTINGS = {
     ],
 }
 
+
 # OTP
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS').lower() == 'true'
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL').lower() == 'true'
